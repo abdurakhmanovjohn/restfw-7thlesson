f7121f945e1c9ca25725bef80987e10ed98c6de0
Jonibek <chartakiy@gmail.com>
2025-12-25 00:32:12 +0500

    done

---
+++ usersapp/models.py
@@ -0,0 +1,79 @@
+# pyright: reportIncompatibleVariableOverride=false
+# pyright: reportIncompatibleMethodOverride=false
+# pyright: reportArgumentType=false
+# pyright: reportAttributeAccessIssue=false
+
+import random
+from datetime import datetime, timedelta
+
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import FileExtensionValidator
+from django.db import models
+
+from sharedapp.models import BaseModel
+
+ORDINARY_USER, MANAGER, ADMIN = ("ordinary_user", "admin", "manager")
+NEW, CODE_VERIFIED, DONE, PHOTO_DONE = ("new", "code_verified", "done", "photo_done")
+VIA_EMAIL, VIA_PHONE = ("via_email", "via_phone")
+
+
+class User(AbstractUser, BaseModel):
+    USER_ROLE = ((ORDINARY_USER, ORDINARY_USER), (MANAGER, MANAGER), (ADMIN, ADMIN))
+    AUTH_STATUS = (
+        (NEW, NEW),
+        (CODE_VERIFIED, CODE_VERIFIED),
+        (DONE, DONE),
+        (PHOTO_DONE, PHOTO_DONE),
+    )
+    AUTH_TYPE = ((VIA_EMAIL, VIA_EMAIL), (VIA_PHONE, VIA_PHONE))
+
+    user_role = models.CharField(
+        max_length=29, choices=USER_ROLE, default=ORDINARY_USER
+    )
+    auth_status = models.CharField(max_length=29, choices=AUTH_STATUS, default=NEW)
+    auth_type = models.CharField(max_length=29, choices=AUTH_TYPE)
+    phone_number = models.CharField(max_length=13, null=True, blank=True, unique=True)
+    email = models.EmailField(unique=True, blank=True, null=True)
+    photo = models.ImageField(
+        upload_to="users_photo/",
+        default="users_photo/default_user.png",
+        null=True,
+        blank=True,
+        validators=FileExtensionValidator(
+            allowed_extensions=["png", "jpg", "jpeg", "heic"]
+        ),
+    )
+
+    def __str__(self):
+        return self.username
+
+    def generate_code(self, verify_type):
+        code = "".join([str(random.randint(0, 100) for _ in range(4))])
+        UserConfirmation.objects.create(
+            user_id=self.id, code=code, verify_type=verify_type
+        )
+        return code
+
+
+EXPIRATION_EMAIL = 3
+EXPIRATION_PHONE = 2
+
+
+class UserConfirmation(BaseModel):
+    VERIFY_TYPE = ((VIA_EMAIL, VIA_EMAIL), (VIA_PHONE, VIA_PHONE))
+    code = models.CharField(max_length=4)
+    verify_type = models.CharField(max_length=29, choices=VERIFY_TYPE)
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    expiration_time = models.DateTimeField()
+    confirmed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.user.__str__())
+
+    def save(self, *args, **kwargs):
+        if self.verify_type == VIA_EMAIL:
+            self.expiration_time = datetime.now() + timedelta(minutes=EXPIRATION_EMAIL)
+        else:
+            self.expiration_time = datetime.now() + timedelta(minutes=EXPIRATION_PHONE)
+
+        super(UserConfirmation, self).save(*args, **kwargs)
